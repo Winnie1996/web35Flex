6faad0876fc68acff77cf080f7e2dd7adc1e8d9a
HungT <64987549+Winnie1996@users.noreply.github.com>
2020-08-31 12:23:07 -0700

    Added Everything but append and color

--- js/index.js
+++ js/index.js
@@ -56,12 +56,56 @@ navLinks[3].textContent = siteContent["nav"]["nav-item-4"];
 navLinks[4].textContent = siteContent["nav"]["nav-item-5"];
 navLinks[5].textContent = siteContent["nav"]["nav-item-6"];
 
+//Add Circle Img
+
 let circleImg = document.getElementById("cta-img");
 circleImg.setAttribute("src", siteContent["cta"]["img-src"]);
 
+//Add H1 Text
 let domText = document.querySelector(".cta-text h1");
 // domText.textContent = siteContent["cta"]["h1"];
 domText.innerHTML = "DOM <br> Is <br> Awesome";
 
+//Add Button Text
 let buttonText = document.querySelector(".cta-text button");
 buttonText.textContent = siteContent["cta"]["button"];
+
+//Top Content
+let headerNames = document.querySelectorAll(".top-content h4");
+headerNames[0].textContent = siteContent["main-content"]["features-h4"];
+headerNames[1].textContent = siteContent["main-content"]["about-h4"];
+
+//Add Paragraphs
+let topParagraph = document.querySelectorAll(".top-content p");
+topParagraph[0].textContent = siteContent["main-content"]["features-content"];
+topParagraph[1].textContent = siteContent["main-content"]["about-content"];
+
+//Add Rectangle Img
+let rectangleImg = document.querySelector(".middle-img");
+rectangleImg.setAttribute("src", siteContent["main-content"]["middle-img-src"]);
+
+//Bottom Content
+let bottomHeaderNames = document.querySelectorAll(".bottom-content h4");
+bottomHeaderNames[0].textContent = siteContent["main-content"]["services-h4"];
+bottomHeaderNames[1].textContent = siteContent["main-content"]["product-h4"];
+bottomHeaderNames[2].textContent = siteContent["main-content"]["vision-h4"];
+
+//Bottom Paragraphs
+let bottomParagraph = document.querySelectorAll(".bottom-content p");
+bottomParagraph[0].textContent =
+  siteContent["main-content"]["services-content"];
+bottomParagraph[1].textContent = siteContent["main-content"]["product-content"];
+bottomParagraph[2].textContent = siteContent["main-content"]["vision-content"];
+
+//Add Contacts
+let contact = document.querySelector(".contact h4");
+contact.textContent = siteContent["contact"]["contact-h4"];
+
+//Add Contact Info
+let contactInfo = document.querySelectorAll(".contact p");
+contactInfo[0].textContent = siteContent["contact"]["address"];
+contactInfo[1].textContent = siteContent["contact"]["phone"];
+contactInfo[2].textContent = siteContent["contact"]["email"];
+
+let footer = document.querySelector("footer p");
+footer.textContent = siteContent["footer"]["copyright"];
